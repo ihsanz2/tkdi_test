0af18a553e1ac88166bc512cf1fd041de6ca4826
ihsanz2 <mhd.ihsan99@gmail.com>
2018-10-03 16:40:29 +0700

    inital commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(".slider").slider({
+    indicators: false,
+    height: 700,
+    transition: 200,
+    interval: 4000
+  });
+  $(".sidenav").sidenav();
+
+  $(".carousel").carousel({
+    dist: 0,
+    padding: 0,
+    // fullWidth: true,
+    indicators: true,
+    duration: 100
+  });
+  setTimeout(autoplay, 7000);
+  function autoplay() {
+    $(".carousel").carousel("next");
+    setTimeout(autoplay, 7000);
+  }
+
+  $(".modal").modal();
+  $(".scrollspy").scrollSpy({
+    scrollOffset: 75
+  });
+  AOS.init();
+});
